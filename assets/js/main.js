0d2304e2fe4d146bafa325c89869ae78d01c4d7f
babadav <babadav@gmail.com>
2017-08-15 13:26:59 -0400

    in class

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,17 +41,83 @@ new Waypoint({
   }
 });
 
+new Waypoint({
+  element: document.querySelector('.text-block-1'),
+  offset: "10%",
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!', direction)
+    if (direction == "down") {
+      // do animation
+      
+      TweenMax.to(".tb1-left-animation", 3, {
+      left: "25%",
+      top: "10%"
+    });
+      TweenMax.to(".tb1-right-animation", 3, {
+      right: "10%",
+      top: "60%"
+    });
+    }
+  }
+});
 
-TweenMax.to(".tb1-left-animation", 3, {
+new Waypoint({
+  element: document.querySelector('.text-block-2'),
+  offset: "10%",
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!', direction)
+    if (direction == "down") {
+      // do animation
+      
+      TweenMax.to(".tb2-left-animation", 3, {
+      left: "15%",
+      top: "10%"
+    });
+      TweenMax.to(".tb2-right-animation", 3, {
+      right: "1%",
+      top: "100%"
+    });
+    }
+  }
+});
 
-			left: "20%",
-			top: "10%"
-		});
 
-TweenMax.to(".tb1-right-animation", 3, {
-			right: "40%",
-			top: "60%"
-		});
+new Waypoint({
+  element: document.querySelector('.text-block-4'),
+  offset: "100%",
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!', direction)
+    if (direction == "down") {
+      // do animation
+      
+      TweenMax.to(".tb4-left-animation", 3, {
+      left: "15%",
+      top: "10%"
+    });
+      TweenMax.to(".tb4-right-animation", 3, {
+      right: "43%",
+      top: "-20%"
+    });
+    }
+  }
+});
+
+
+
+var yearContainer = document.querySelector('.year');
+
+yearContainer.addEventListener('click' , function(){
+  console.log('click');
+
+  yearContainer.classList.add('year-active');
+
+  if (yearContainer.classList = 'year-active') {
+    yearContainer.classList.add('year');
+    yearContainer.classList.remove('year-active');
+
+  }
+});
+
 
 
 
